f37ca84888fdbe1c33fa52b6a521df1b5cd3e33f
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-28 08:48:25 +0200

    OwlsNest: QS panel opacity (2/2)
    Change-Id: Iad2942bfc17d1802a061f334bfa54796137ba0d9
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/aosip/owlsnest/quicksettings/QuickSettingsHolder.java
+++ src/com/aosip/owlsnest/quicksettings/QuickSettingsHolder.java
@@ -45,7 +45,9 @@ public class QuickSettingsHolder extends SettingsPreferenceFragment implements
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+    private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
 
+    private CustomSeekBarPreference mQsPanelAlpha;
     private CustomSeekBarPreference mQsRowsPort;
     private CustomSeekBarPreference mQsRowsLand;
     private CustomSeekBarPreference mQsColumnsPort;
@@ -119,6 +121,13 @@ public class QuickSettingsHolder extends SettingsPreferenceFragment implements
         mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
         updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
         mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
+
+        mQsPanelAlpha = (CustomSeekBarPreference) findPreference(KEY_QS_PANEL_ALPHA);
+        int qsPanelAlpha = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_PANEL_BG_ALPHA, 221);
+        mQsPanelAlpha.setValue((int)(((double) qsPanelAlpha / 255) * 100));
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -176,7 +185,13 @@ public class QuickSettingsHolder extends SettingsPreferenceFragment implements
                     tileAnimationInterpolator, UserHandle.USER_CURRENT);
             updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
             return true;
-       }
+        } else if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            int trueValue = (int) (((double) bgAlpha / 100) * 255);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_PANEL_BG_ALPHA, trueValue);
+            return true;
+        }
 
         return false;
     }
